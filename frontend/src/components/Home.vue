0d03628c2717ff82f8ccf0f80bb77c5f6e904d89
Giovanni Oliveira <giovanni.oliveira@logos-ma.com.br>
2018-11-13 18:02:21 -0300

    Update Home, move lists to each subject page.

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,55 +1,59 @@
 <template>
   <div>
-    <v-layout>
-      <v-flex >
-        <list :headers="course_headers" :items="courses"></list>
-      </v-flex>
-      <v-flex>
-        <list :headers="student_headers" :items="students"></list>
-      </v-flex>
-    </v-layout>
-    
+    <v-content>
+      <v-container mt-5>
+        <div class="grey lighten-3 board">
+          <v-card>
+            <v-container fluid grid-list-lg>
+              <v-layout row wrap>
+                <v-flex xs12 sm6 v-for="(item, index) in items" :key="index">
+                  <v-card :color="item.color" class="white--text">
+                    <v-card-title primary-title>
+                      <div class="headline">{{ item.title }}</div>
+                      <div class="text-xs-left">{{ item.text }}</div>
+                    </v-card-title>
+                    <v-layout>
+                      <v-flex text-xs-right>
+                        <v-btn dark :to="item.route">Acessar</v-btn>
+                      </v-flex>
+                    </v-layout>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </div>
+      </v-container>
+    </v-content>
   </div>
 </template>
 
 <script>
-import List from "./Course/List";
 export default {
-  name: "Home",
-  components: { List },
   data() {
     return {
-      course_headers: [
-          { text: 'Código', value: 'code', align: 'left' },
-          { text: 'Nome', value: 'name' },
-          { text: 'Carga Horária', value: 'hourly_load' },
-          { text: 'Data de Cadastro', value: 'register_date' },
-        ],
-      courses: [
-        { code: 'C01', name: 'ADM', hourly_load: 120, register_date: '20/10/2008' },
-        { code: 'C02', name: 'DIR', hourly_load: 160, register_date: '20/10/2009' },
-        { code: 'C03', name: 'MED', hourly_load: 240, register_date: '20/10/2006' },
-      ],
-      student_headers: [
-        { text: 'Código', value: 'code', align: 'left' },
-        { text: 'Nome', value: 'name' },
-        { text: 'CPF', value: 'cpf' },
-        { text: 'Curso', value: 'course' },
-        ],
-      students: [
-        { code: 'A01', name: 'Pedro', cpf: 12345678910, course: 'Administração' },
-        { code: 'A02', name: 'Amanda', cpf: 12345678912, course: 'Direito' },
-        { code: 'A03', name: 'Fernando', cpf: 12345678911, course: 'Medicina' },
-        { code: 'A04', name: 'Maria', cpf: 12345678906, course: 'Administração' },
-        { code: 'A05', name: 'João', cpf: 12345678917, course: 'Direito' },
-        { code: 'A06', name: 'José', cpf: 12345678915, course: 'Administração' },
-        { code: 'A07', name: 'Francisca', cpf: 12345678665, course: 'Administração' },
-      ],
+      items: [
+        {
+          title: "Cursos",
+          text: "Acesse para gerenciar todas as informações sobre os cursos.",
+          color: "blue darken-3",
+          route: {name: "Courses"}
+        },
+        {
+          title: "Alunos",
+          text: "Acesse para gerenciar as informações sobre os alunos matriculados.",
+          color: "red darken-2",
+          route: {name: "Students"}
+        },
+      ]
     };
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.board {
+  max-width: 800px;
+  margin: auto;
+}
 </style>
