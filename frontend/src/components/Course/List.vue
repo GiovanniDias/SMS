bbcc803aa4f6110b1b002a2f70b9d244ad5987d3
Giovanni Oliveira <giovanni.oliveira@logos-ma.com.br>
2018-11-13 15:05:01 -0300

    Build list component to list registered items.

---
+++ frontend/src/components/Course/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <v-data-table
+          :headers="headers"
+          :items="items"
+          class="elevation-2"
+        >
+          <template slot="items" slot-scope="props">
+            
+            <td class="text-xs-left"
+              v-for="(attribute, a) of props.item" :key="a"
+            > {{ attribute }} </td>
+
+          </template>
+        </v-data-table>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'List',
+    props: ['headers', 'items'],
+  }
+</script>
