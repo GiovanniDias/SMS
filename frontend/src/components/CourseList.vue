710d3067da8f2aed58c2e8bdceb25c4d200bb345
Giovanni Oliveira <giovanni.oliveira@logos-ma.com.br>
2018-11-20 09:25:58 -0300

    Add Course and Student lists in isolated components

---
+++ frontend/src/components/CourseList.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    class="elevation-2"
+  >
+    <template slot="items" slot-scope="props">
+      <td class="text-xs-left"> {{ props.item.code }} </td>
+      <td class="text-xs-left"> {{ props.item.name }} </td>
+      <td class="text-xs-left"> {{ props.item.hourly_load }} </td>
+      <td class="text-xs-left"> {{ props.item.register_date }} </td>
+      <td class="text-xs-left">
+        <v-icon small class="mr-2" @click="editItem(props.item)">edit</v-icon>
+        <v-icon small @click="deleteItem(props.item)">delete</v-icon>
+      </td>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: "CourseList",
+  props: ["headers", "items"]
+};
+</script>
+
---
+++ frontend/src/components/StudentList.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    class="elevation-2"
+  >
+    <template slot="items" slot-scope="props">
+      <td class="text-xs-left"> {{ props.item.code }} </td>
+      <td class="text-xs-left"> {{ props.item.name }} </td>
+      <td class="text-xs-left"> {{ props.item.cpf }} </td>
+      <td class="text-xs-left"> {{ props.item.course }} </td>
+      <td class="text-xs-left">
+        <v-icon small class="mr-2" @click="editItem(props.item)">edit</v-icon>
+        <v-icon small @click="deleteItem(props.item)">delete</v-icon>
+      </td>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: "StudentList",
+  props: ["headers", "items"]
+};
+</script>
